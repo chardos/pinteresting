b74760300b56ff6824de42ab41d77170173be9cd
Richard Tan <chardos@gmail.com>
2014-12-23 00:37:43 +1100

    added paperclip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,8 @@ Rails.application.configure do
 
   #Required for Heroku
   config.action_mailer.default_url_options = { :host => 'chard-pinteresting.herokuapp.com' }
+
+
+  Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
+  Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
 end
