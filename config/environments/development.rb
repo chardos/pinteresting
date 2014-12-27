5e862ea26055411df76c52e653e501355ecf7141
Richard Tan <chardos@gmail.com>
2014-12-27 15:41:21 +1100

    added jquery masonry

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'chard-pinteresting.herokuapp.com' }
 
 
+
+
   Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
   Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
 end
